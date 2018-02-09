6b14903dc56346a3ef95d8a307c26e1042484185
Jonny Quarta <quarta@epsitec.ch>
2018-02-09 13:24:14 +0100

    Add translations resources for validation errors

--- src/elm/Translations/Resources.elm
+++ src/elm/Translations/Resources.elm
@@ -3,5 +3,12 @@ module Translations.Resources exposing (..)
 
 type Resource =
   RTitle | RDropYourCode | RWeWillValidateIt |
+
   RErrMultipleFilesDropped | RErrNetworkError | RErrUnknownError |
-  RErrInvalidEncoding | RErrInvalidInvoiceImage | RErrGenerationError | RErrValidationError
+  RErrInvalidEncoding | RErrInvalidInvoiceImage | RErrGenerationError | RErrValidationError |
+
+  RValErrDoesNotExist | RValErrIsEmpty | RValErrMustBeEmpty | RValErrMustBeEqualTo |
+  RValErrLengthDifferent | RValErrLengthExceeded | RValErrLengthNotReached | RValErrMustBeDifferentThan |
+  RValErrDoesNotStartWith | RValErrInvalid | RValErrFormatIsDifferentThan | RValErrQrReferenceInvalid |
+  RValErrCreditorReferenceInvalid | RValErrNoReferenceInvalid | RValErrValueExceeded | RValErrValueNotReached |
+  RValErrInvalidTags | RValErrUnknownTag | RValErrTagNotOrdered | RValErrTagAlreadyExists | RValErrSwiftFormat
