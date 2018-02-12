ace9e03ceda9e597206d8ac0639e09e97c190d8d
Jonny Quarta <quarta@epsitec.ch>
2018-02-12 10:33:47 +0100

    Fix rendering of qr invoice image on any browser

--- src/elm/Components/QrImage.elm
+++ src/elm/Components/QrImage.elm
@@ -11,28 +11,49 @@ renderImage : String -> Html a
 renderImage image =
   div [style [
     ("display", "flex"),
-    ("justify-content", "flex-end"),
-    ("margin-right", "3em"),
-    ("align-items", "center")
+    ("flex-grow", "2"),
+    ("flex-basis", "0"),
+    ("flex-shrink", "0")
     ]]
   [
-    img [src ("data:image/png;base64," ++ image), style [("width", "60%")]] []
+    div [style [
+      ("display", "flex"),
+      ("margin-left", "3em"),
+      ("align-items", "center")
+      ]]
+    [
+      img [src ("data:image/png;base64," ++ image), style [("width", "100%")]] []
+    ]
   ]
 
-
-view : Language -> Backend.WebService.Generation -> Html a
-view language generation =
+renderActions : Html a
+renderActions =
   div [style [
     ("display", "flex"),
     ("flex-grow", "1"),
-    ("justify-content", "center"),
+    ("flex-basis", "0"),
+    ("flex-shrink", "0")
+    ]]
+  []
+
+renderContent : Language -> String -> Html a
+renderContent language img =
+  div [style [
+    ("display", "flex"),
+    ("flex-grow", "1"),
+    ("flex-basis", "0"),
     ("align-items", "stretch")]
   ] [
-    case generation.error of
-      Nothing ->
-        case generation.image of
-          Nothing -> renderSpinner language
-          Just img -> renderImage img
-      Just err ->
-          renderError err language
+    renderImage img,
+    renderActions
   ]
+
+view : Language -> Backend.WebService.Generation -> Html a
+view language generation =
+  case generation.error of
+    Nothing ->
+      case generation.image of
+        Nothing -> renderSpinner language
+        Just img -> renderContent language img
+    Just err ->
+        renderError err language
